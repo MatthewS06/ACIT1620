a109dde162e83a2d67165b159cbfb4b4069f015b
Matthew San Agustin <matthewsanagustin@hotmail.com>
2025-03-13 09:19:58 -0700

    added files from week 5 to week 9

---
+++ week9/mathrandom.js
@@ -0,0 +1,4 @@
+console.log(Math.random()); // outputs between 0,1
+console.log(Math.floor(Math.random() *10)); // outputs within 0, 9
+console.log(Math.floor(10 + Math.random() *90)); // 10 to 99
+// Math.floor(min + Math.random() * max-min+1)
